b39243e52df8a461bbf6703b8033a33c66b1610e
nadvolod <2136245+nadvolod@users.noreply.github.com>
2026-07-07 15:29:17 -0400

    Go track: module 06 course code (same code runs anywhere)

---
+++ go/course-repo/exercise/06-same-code-runs-anywhere/worker/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"standaloneactivities/exercise/06-same-code-runs-anywhere/webhook"
+
+	"go.temporal.io/sdk/client"
+	"go.temporal.io/sdk/worker"
+)
+
+func main() {
+	c, err := client.Dial(client.Options{HostPort: "localhost:7233"})
+	if err != nil {
+		log.Fatalln("Unable to create client", err)
+	}
+	defer c.Close()
+
+	w := worker.New(c, webhook.TaskQueue, worker.Options{})
+	w.RegisterActivity(webhook.DeliverWebhook)
+	w.RegisterWorkflow(webhook.WebhookWorkflow)
+
+	log.Printf("Worker running on task queue %q", webhook.TaskQueue)
+	if err := w.Run(worker.InterruptCh()); err != nil {
+		log.Fatalln("Unable to start worker", err)
+	}
+}
